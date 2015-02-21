d1a16e83af41abda6e999dd4e69c2b7ad3a03f50
AndreiNegru <andrei.negru@gmail.com>
2015-02-21 09:56:13 +0200

    my first commit

--- src/AgendaAsCollection.java
+++ src/AgendaAsCollection.java
@@ -24,6 +24,7 @@ public class AgendaAsCollection {
 
         do {
             m.printMenu();
+
             int option = m.readMenuOption();
             switch (option) {
                 case 1:
